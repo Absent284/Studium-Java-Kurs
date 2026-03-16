5f6b32feaa3c66c05b0b1b3a9cbafdc9e5720733
Absent284 <robert.bild@gmx.net>
2026-03-16 10:45:24 +0100

    Kapitelupdate

--- src/SchlüsselwortIF.java
+++ src/SchlüsselwortIF.java
@@ -3,9 +3,38 @@ public class SchlüsselwortIF {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
 		
-		System.out.println("Das ist der Test ob ich GIT und Eclipse verlinked bekomme.");
+			// Allgemeine Punktzahl
+			int score = 4000;
+			
+			if (score == 5000) {
+				System.out.println("Herzlichen Glückwunsch, neuer Rekord!!");
+			}else {
+				System.out.println("Schade dein Score reicht leider nicht!");
+			}
+
+			// Spielerleben
+			int lebenPlayer = 3;
+			if (lebenPlayer == 0) {
+				System.out.println("Spiel verloren");
+				
+			}
+			
+			// Trefferlogik
+			boolean gegnerHit = true;
+			int highscore = 0;
+			
+			if (gegnerHit == true) {
+				System.out.println("Gegner getroffen!");
+				highscore += 10;
+				System.out.println("Highscore ist: " + highscore);
+			} else {
+				System.out.println("Ziel verfehlt");
+			}
+			
+			
+				
+		
 	}
 
 }
