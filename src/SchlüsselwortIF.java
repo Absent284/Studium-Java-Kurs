11ed2131da3201306fb0b1f7343ae8de75689c25
Absent284 <robert.bild@gmx.net>
2026-03-16 10:20:11 +0100

    Initialer Push

---
+++ src/SchlüsselwortIF.java
@@ -0,0 +1,11 @@
+
+public class SchlüsselwortIF {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("Das ist der Test ob ich GIT und Eclipse verlinked bekomme.");
+	}
+
+}
